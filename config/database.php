cdc524a8ee6438170d21622b7c8ac86c98f5b8e5
Mamadou Harouna Diallo <36333565+hadpro24@users.noreply.github.com>
2019-01-20 21:51:19 +0100

    changement des config database

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'eventbrote');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_HOST', 'ec2-54-235-77-0.compute-1.amazonaws.com');
+define('DB_NAME', 'dbrgds4vk0gpup');
+define('DB_USERNAME', 'oeggtoeibmdmpw');
+define('DB_PASSWORD', '4659736a15e973a4402b5318b08f1d964e73d81e330a3ba3e4d31d556bb445dd');
 
 try
 {
-    $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, 
+    $db = new PDO("pgsql:host=".DB_HOST.";port=5432;dbname=".DB_NAME, 
            DB_USERNAME, DB_PASSWORD);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
