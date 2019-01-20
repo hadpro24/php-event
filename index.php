7c19126fafa75085d9153ea773d33e18aa63e7e8
Mamadou Harouna Diallo <36333565+hadpro24@users.noreply.github.com>
2019-01-20 22:14:03 +0100

    create event

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 
 include_once('config/database.php'); //connexion à la base de donnée
 
+
+$query = $db->query("CREATE SEQUENCE event_seq;
+CREATE TABLE event(
+id int primary key default nextval ('event_seq'),
+title varchar(255),
+content text,
+id_user int
+);");
+
 $query = $db->query("SELECT * FROM event");
 $data = $query->fetchAll(PDO::FETCH_OBJ);
 
