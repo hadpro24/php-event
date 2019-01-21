04400ec6dad469ed1841200fbcd9f9216944a710
Harouna Diallo <mhdiallo@misgroupe.com>
2019-01-21 19:06:58 +0000

    changing colone

--- profile.php
+++ profile.php
@@ -24,7 +24,7 @@ if(isset($_POST['register']))
         if(count($errors) == 0)
         {
             //il n'a pas d'erreur alors on enregistre les infos
-            $q = $db->prepare("INSERT INTO event(id_users, title, contenu, date_event, adresse_event, created_at) 
+            $q = $db->prepare("INSERT INTO event(id_users, title, contenu, date_event, adresse_event, create_at) 
                         VALUES(:id_users, :titre, :contenu, :date_event, :adresse_event, NOW())");
             $q->execute([
                 'id_users' => $_SESSION['user_id'],
